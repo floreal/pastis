bded51b275a1e7b234da87076b473094d20d1def
Floréal TOUMIKIAN <ftoumikian@april.org>
2011-08-01 16:49:12 +0200

    Project Initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pastis::Application.config.secret_token = '53e73c319778083999d47bbfaa6b402a405ef469da636eab6df86e4882c829a607761b867cb756467765caeba30bafa7f9a21ddff925435f0fcb1d913cfffe68'
