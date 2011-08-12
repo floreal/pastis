37f4a1e80b1b20290a586acd7afc33e6b8d0f375
Floréal TOUMIKIAN <ftoumikian@april.org>
2011-08-12 11:49:04 +0200

    Added Server side lookup validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, :with => :render_not_found
+  before_filter :retreive_lookup_object
+  
+  def retreive_lookup_object
+    @lookup = (flash[:lookup] || Lookup.new)
+  end
   
   def render_not_found(exception)
     @title = "Status 404: Sinppet not found"
--- app/controllers/lookup_controller.rb
+++ app/controllers/lookup_controller.rb
@@ -2,11 +2,14 @@ class LookupController < ApplicationController
   def find
     if request.post?
       lookup = Lookup.new params[:lookup]
+      if lookup.valid?
       redirect_to :controller => :snippets,
                   :action => :show,
                   :id => lookup.snippet_id
-    else
-      render :file => '/public/404.html', :status => 404, :layout => false
+      else
+        flash[:lookup] = lookup
+        redirect_to request.referer
+      end
     end
   end
 end
